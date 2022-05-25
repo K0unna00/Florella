da324dc6008585a1ffe3048cde38295d33233fae
Baxtiyar <100702860+K0unna00@users.noreply.github.com>
2022-05-25 17:35:37 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// $(function(){
+//     $("a").mouseenter(function(){
+//         let div = this.nextElementSibling;
+//         div.style.height="300px";
+//     });
+//     $("a").mouseleave(function(){
+//         let div = this.nextElementSibling;
+//         div.style.height="0px";
+//     });
+//   });
+
+
+  
